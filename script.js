54299eb8c8254801340ef7da1d2100503295d331
Patdu-G <patriogabrielpatdu@gmail.com>
2026-09-08 21:47:06 +0800

    changes in testament

--- script.js
+++ script.js
@@ -355,7 +355,7 @@ if (aboutTerminalTarget) {
       id: 'testi-body-1',
       name: 'Groupmate',
       role: 'Student Developer',
-      quote: 'Super reliable teammate. Whenever we had a project, Gab made sure the code was clean and the design was on point.',
+      quote: 'Super reliable teammate. Gab made sure the code was clean and the design was on point.',
     },
     {
       id: 'testi-body-2',
